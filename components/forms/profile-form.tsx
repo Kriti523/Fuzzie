5deac1234d372f32112c7920c5529236bd64c2da
Kriti523 <kritijain9414@gmail.com>
2024-10-07 21:38:34 +0530

    landing page + side bar

---
+++ components/forms/profile-form.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React, { useState } from 'react'
+
+type Props = {}
+
+const profileForm = (props: Props) => {
+    const [isLoading, setisLoading] = useState(false)
+  return (
+    <div>profileForm</div>
+  )
+}
+
+export default profileForm
